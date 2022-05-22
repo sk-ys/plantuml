41c8e700ed0ba7fcfc6c7d39e2b59c0518916ae1
sk-ys <yskrmt@gmail.com>
2022-05-22 19:08:19 +0900

    support HTTP server

--- app/helpers/plantuml_helper.rb
+++ app/helpers/plantuml_helper.rb
@@ -1,4 +1,5 @@
 require 'digest/sha2'
+require 'uri'
 
 module PlantumlHelper
   ALLOWED_FORMATS = {
@@ -19,9 +20,19 @@ module PlantumlHelper
   end
 
   def self.plantuml(text, args)
+    settings_binary = Setting.plugin_plantuml['plantuml_path']
+    if Regexp.compile("^http").match(settings_binary)
+      name = plantuml_server(text, args)
+    else
+      name = plantuml_local(text, args)
+    end
+    name
+  end
+
+  def self.plantuml_local(text, args)
     frmt = check_format(args)
     name = construct_cache_key(sanitize_plantuml(text))
-    settings_binary = Setting.plugin_plantuml['plantuml_binary_default']
+    settings_binary = Setting.plugin_plantuml['plantuml_path']
     unless File.file?(plantuml_file(name, '.pu'))
       File.open(plantuml_file(name, '.pu'), 'w') do |file|
         file.write "@startuml\n"
@@ -35,9 +46,58 @@ module PlantumlHelper
     name
   end
 
+  def self.plantuml_server(text, args)
+    server_url = Setting.plugin_plantuml['plantuml_path']
+    server_url << '/' if !server_url.end_with?('/')
+    frmt = check_format(args)
+    name = construct_cache_key(sanitize_plantuml(text))
+    text_encoded = encode64(text)
+
+    # If using HUFFMAN encoding, need to add ~1 to the header.
+    url = URI.join(server_url, "#{frmt[:type]}/~1#{text_encoded}").to_s
+  end
+
   def self.sanitize_plantuml(text)
     return text if Setting.plugin_plantuml['allow_includes']
     text.gsub!(/!include.*$/, '')
     text
+  end
+
+  # ref. https://plantuml.com/code-javascript-synchronous
+  def self.encode64(text)
+    compressed = Zlib::Deflate.deflate(text, Zlib::BEST_COMPRESSION)
+    compressed.chars.each_slice(3).map do |chars|
+      append3bytes(chars[0].ord, chars[1]&.ord.to_i, chars[2]&.ord.to_i)
+    end.join
+  end
+
+  def self.encode6bit(b)
+    if (b < 10) 
+        return (48 + b).chr
+    end
+    b -= 10
+    if (b < 26) 
+        return (65 + b).chr
+    end
+    b -= 26
+    if (b < 26) 
+        return (97 + b).chr
+    end
+    b -= 26
+    if (b == 0) 
+        return '-'
+    elsif (b == 1) 
+        return '_'
+    end
+    return '?'
+  end
+
+  def self.append3bytes(b1, b2, b3)
+    [
+      b1 >> 2,
+      ((b1 & 0x3) << 4) | (b2 >> 4),
+      ((b2 & 0xF) << 2) | (b3 >> 6),
+      b3 & 0x3F,
+    ].map { |c| self.encode6bit(c & 0x3F) }.join
   end
 end
--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :plantuml do
   requires_redmine version: '2.6'..'4.2'
 
   settings(partial: 'settings/plantuml',
-           default: { 'plantuml_binary' => {}, 'cache_seconds' => '0', 'allow_includes' => false })
+           default: { 'plantuml_path' => {}, 'cache_seconds' => '0', 'allow_includes' => false })
 
   Redmine::WikiFormatting::Macros.register do
     desc <<EOF
@@ -23,12 +23,20 @@ Redmine::Plugin.register :plantuml do
       ** (png|svg)
 EOF
     macro :plantuml do |obj, args, text|
-      raise 'No PlantUML binary set.' if Setting.plugin_plantuml['plantuml_binary_default'].blank?
+      copy_old_setting
+      raise 'No PlantUML binary set.' if Setting.plugin_plantuml['plantuml_path'].blank?
       raise 'No or bad arguments.' if args.size != 1
       frmt = PlantumlHelper.check_format(args.first)
       image = PlantumlHelper.plantuml(text, args.first)
-      image_tag "/plantuml/#{frmt[:type]}/#{image}#{frmt[:ext]}"
+
+      if Regexp.compile("^http").match(image)
+        image_tag image
+      else
+        image_tag "/plantuml/#{frmt[:type]}/#{image}#{frmt[:ext]}"
+      end
     end
+
+
   end
 end
 
@@ -38,5 +46,18 @@ Rails.configuration.to_prepare do
 
   unless Redmine::WikiFormatting::Textile::Helper.included_modules.include? PlantumlHelperPatch
     Redmine::WikiFormatting::Textile::Helper.send(:include, PlantumlHelperPatch)
+  end
+  if (Redmine::VERSION::MAJOR == 3 && Redmine::VERSION::MINOR >= 1) || Redmine::VERSION::MAJOR >= 4
+    unless Redmine::WikiFormatting::Markdown::Helper.included_modules.include? PlantumlHelperPatch
+      Redmine::WikiFormatting::Markdown::Helper.send(:include, PlantumlHelperPatch)
+    end
+  end
+end
+
+private
+
+def copy_old_setting
+  if Setting.plugin_plantuml['plantuml_path'].blank?
+    Setting.plugin_plantuml['plantuml_path'] = Setting.plugin_plantuml['plantuml_binary_default']
   end
 end
